27fd0c68a7a634b9911fd7fb4b6804612d0c96ae
Ofekmeister <ofekmeister@gmail.com>
2015-05-17 20:00:09 -0400

    fixes

--- install.py
+++ install.py
@@ -46,7 +46,7 @@ class PhantomJSInstaller:
         self.install_deps_cmd = []
 
         self.win_base_dir = 'C:/phantomjs/'
-        self.nix_base_dir = '/usr/lib/'
+        self.nix_base_dir = '/usr/lib/phantomjs/'
 
         if os.name == 'nt' or platform.system() == 'Windows':
             try:
@@ -61,14 +61,14 @@ class PhantomJSInstaller:
             except:
                 pass
             self.url = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-macosx.zip'
-            self.executable_path = os.path.join(self.nix_base_dir, 'phantomjs/phantomjs')
+            self.executable_path = os.path.join(self.nix_base_dir, 'phantomjs')
         elif os.name == 'posix' or platform.system() == 'Linux':
             try:
                 os.mkdir(self.nix_base_dir)
             except:
                 pass
             linux_name = platform.linux_distribution()[0]
-            self.executable_path = os.path.join(self.nix_base_dir, 'phantomjs/phantomjs')
+            self.executable_path = os.path.join(self.nix_base_dir, 'phantomjs')
             if linux_name in ('debian', 'Ubuntu',):
                 self.install_deps_cmd.extend(('sudo', 'apt-get', 'install',))
                 self.install_deps_cmd.extend(self.debian_deps)
@@ -76,8 +76,13 @@ class PhantomJSInstaller:
                 self.install_deps_cmd.extend(('sudo', 'yum', '-y', 'install',))
                 self.install_deps_cmd.extend(self.fedora_deps)
 
+        try:
+            os.remove(self.executable_path)
+        except:
+            pass
+
     def fetch_archive(self):
-        print('\nFetching archive...\n\n')
+        print('\n\nFetching archive...\n')
 
         req = urlopen(self.url)
         with open(self.archive_path, 'wb') as archive:
@@ -90,7 +95,7 @@ class PhantomJSInstaller:
                     break
 
     def extract_archive(self):
-        print('Extracting executable from archive...\n\n')
+        print('\nExtracting executable from archive...\n')
 
         with ZipFile(self.archive_path) as archive,\
                 open(self.executable_path, 'wb') as executable:
@@ -100,11 +105,11 @@ class PhantomJSInstaller:
                     break
 
     def install_deps(self):
-        print('Installing dependencies...\n\n')
+        print('\n\nInstalling dependencies...\n')
         subprocess.call(self.install_deps_cmd)
 
     def build(self):
-        print('Compiling...\n\n')
+        print('\nCompiling...\n')
 
         for cmd in self.build_cmds:
             print('\n>>> Running command: {}\n\n'.format(' '.join(cmd)))
@@ -114,21 +119,39 @@ class PhantomJSInstaller:
             subprocess.call(cmd)
 
     def cleanup(self):
-        os.chdir(os.path.dirname(self.executable_path))
-        shutil.rmtree(self.temp_dir)
+        try:
+            os.chdir(os.path.dirname(self.executable_path))
+            shutil.rmtree(self.temp_dir)
+        except:
+            pass
 
     def run(self):
-        if self.executable_path is not None:
-            if self.url is not None:
-                self.fetch_archive()
-                self.extract_archive()
+        s = ('\n\nTo see how to manually install PhantomJS, please go to\n'
+             'https://github.com/Ofekmeister/katutil\n\n')
+
+        choice = input('\n\nAutomatically install PhantomJS? (y/n)')
+        if choice in ('n', 'no'):
+            print(s)
+            return
+
+        try:
+            if self.executable_path is not None:
+                if self.url is not None:
+                    self.fetch_archive()
+                    self.extract_archive()
+                else:
+                    self.install_deps()
+                    self.build()
+                    shutil.move('bin/phantomjs', self.executable_path)
             else:
-                self.install_deps()
-                self.build()
-                shutil.move('bin/phantomjs', self.executable_path)
+                input('\nUnknown operating system, automatic installation '
+                      'failed.{}'.format(s))
             self.cleanup()
-        else:
-            input('Automatic installation failed')
+        except:
+            input('\nAn error occurred, automatic installation '
+                  'failed.{}'.format(s))
+            self.cleanup()
+            raise
 
 
 if __name__ == '__main__':
